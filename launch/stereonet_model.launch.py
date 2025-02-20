7985c517001dd5298136d16317490bbec2cc7788
huaiyuhorizon <huaiyu.zhang@horizon.cc>
2025-02-20 12:54:35 +0800

    feat(model) change default model

--- launch/stereonet_model.launch.py
+++ launch/stereonet_model.launch.py
@@ -37,7 +37,7 @@ def generate_launch_description():
     stereonet_model_file_path =  os.path.join(
         get_package_share_directory('hobot_stereonet'),
         'config',
-        'model.hbm'
+        'x5baseplus_alldata_woIsaac.bin'
     )
     local_image_path =  os.path.join(
         get_package_share_directory('hobot_stereonet'),
