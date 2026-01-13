d984b9cf78a0dba102a9b05d51f37054c89a0d27
kao.zhu <kao.zhu@d-robotics.cc>
2026-01-13 16:20:07 +0800

    chore(launch): Add mipi_cal_alpha param for mipi cam

--- launch/stereonet_model_web_visual_component.launch.py
+++ launch/stereonet_model_web_visual_component.launch.py
@@ -102,6 +102,7 @@ def generate_launch_description():
             {"rotation": LaunchConfiguration('mipi_rotation')},
             {"gdc_enable": LaunchConfiguration('mipi_gdc_enable')},
             {"frame_id": LaunchConfiguration('frame_id')},
+            {"cal_alpha": LaunchConfiguration('mipi_cal_alpha')},
         ],
         extra_arguments=[{"use_intra_process_comms": True}],
     )
@@ -263,6 +264,10 @@ def generate_launch_description():
                 default_value="True",
                 description="mipi camera gdc enable",
             ),
+            DeclareLaunchArgument(
+                'mipi_cal_alpha',
+                default_value='0.0',
+                description='mipi camera param of alpha for stereoRectify'),
             stereonet_pub_web_arg,
             target_container_name_arg,
             shared_mem_node,
